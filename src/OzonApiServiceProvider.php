3e68fd9b2aab8a4683df27d251069d938cac99c0
trigold <422520625@qq.com>
2023-01-29 11:27:32 +0800

    init

--- src/OzonApiServiceProvider.php
+++ src/OzonApiServiceProvider.php
@@ -1,7 +1,19 @@
 <?php
 namespace Trigold\OzonApi;
 
-class OzonApiServiceProvider
-{
+use Illuminate\Support\ServiceProvider;
 
+class OzonApiServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/../config/ozon-api.php' => config_path('ozon-api.php'),
+        ]);
+    }
+
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__ . '/../config/ozon-api.php', 'ozon-api');
+    }
 }
