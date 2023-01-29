a8a5c61a3be2949f3543425fa93d2f5e26ec3204
trigold <422520625@qq.com>
2023-01-29 16:02:13 +0800

    init

--- src/Facades/OzonApi.php
+++ src/Facades/OzonApi.php
@@ -6,6 +6,8 @@
  * @document https://github.com/422520625/ozon-api/blob/master/README.md
  * @contact  422520625@qq.com
  */
+namespace Trigold\OzonApi\Facades;
+
 use Illuminate\Support\Facades\Facade;
 
 class OzonApi extends Facade
