3ab3f14b1d0b894db7fb23db38bec3ebcce57b0d
trigold <422520625@qq.com>
2023-01-29 16:37:07 +0800

    init

--- src/App.php
+++ src/App.php
@@ -8,41 +8,21 @@
  */
 namespace Trigold\OzonApi;
 
-use Illuminate\Http\Client\PendingRequest;
 use Illuminate\Support\Facades\Http;
 
 class App
 {
-    private string $baseUri;
-
-    private string $clientId;
-
-    private string $apiKey;
-
-    private PendingRequest $client;
-
-    public function __construct(string $baseUri, string $clientId, string $apiKey)
+    public function __construct()
     {
-        $this->baseUri = $baseUri;
-        $this->clientId = $clientId;
-        $this->apiKey = $apiKey;
-        $this->client = $this->initHttpClient();
-    }
-
-    protected function initHttpClient(): PendingRequest
-    {
-        return Http::baseUrl($this->baseUri)
-            ->withOptions([
-                'timeout' => 10,
-            ])
-            ->withHeaders([
-                'Client-Id' => $this->clientId,
-                'Api-Key' => $this->apiKey,
-            ]);
     }
 
     public function get(string $url, array $query = []): array
     {
-        return $this->client->get($url, $query)->json();
+        return Http::OzonApi()->get($url, $query)->json();
+    }
+
+    public function post(string $url, array $query = []): array
+    {
+        return Http::OzonApi()->post($url, $query)->json();
     }
 }
